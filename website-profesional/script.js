677eb75d8aa85460f9de381b10acb3c672c23c66
Engga Setya Dewi <engga@email.com>
2026-07-15 23:02:08 +0700

    Tambah tombol JavaScript

---
+++ website-profesional/script.js
@@ -0,0 +1,12 @@
+function hubungi(){
+    window.open(
+        "https://wa.me/6283195289657",
+        "_blank"
+function salam() {
+    alert("Halo Engga Setya Dewi 🌸");
+<button onclick="salam()">
+🌸 Klik Saya
+</button>
+}c
+    );
+}
